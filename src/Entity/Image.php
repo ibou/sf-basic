b6f440454cb78baba55d07dde2b7ced6af6b1c85
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-09-08 02:46:00 +0200

    update image class

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -13,7 +13,10 @@ use App\Controller\UploadImageAction;
  * @ORM\Entity()
  * @Vich\Uploadable()
  * @ApiResource(
- *     attributes={"order"={"id": "ASC"}},
+ *     attributes={
+ *         "order"={"id": "ASC"},
+ *         "formats"={"json", "jsonld", "form"={"multipart/form-data"}}
+ *     },
  *     collectionOperations={
  *         "get",
  *         "post"={
@@ -21,6 +24,12 @@ use App\Controller\UploadImageAction;
  *             "path"="/images",
  *             "controller"=UploadImageAction::class,
  *             "defaults"={"_api_receive"=false}
+ *         }
+ *     },
+ *     itemOperations={
+ *         "get",
+ *         "delete"={
+ *             "access_control"="is_granted('ROLE_WRITER')"
  *         }
  *     }
  * )
@@ -71,9 +80,8 @@ class Image
         $this->url = $url;
     }
 
-
-    public function __toString(): string
+    public function __toString()
     {
-        return $this->url;
+        return $this->id . ':' . $this->url;
     }
 }
